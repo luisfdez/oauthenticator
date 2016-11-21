60df4bc849ea375c41344c3ea942e760baaaa9bd
Luis Fernandez Alvarez <luis.fernandezalvarez@epfl.ch>
2016-11-21 16:20:53 +0100

    Testing kubernetes

--- example/jupyterhub_config.py
+++ example/jupyterhub_config.py
@@ -4,3 +4,12 @@ c = get_config()
 
 c.JupyterHub.log_level = 10
 c.JupyterHub.authenticator_class = 'oauthenticator.openshift.OpenShiftOAuthenticator'
+
+c.JupyterHub.spawner_class = 'kubernetes_spawner.KubernetesSpawner'
+c.KubernetesSpawner.verify_ssl = False
+#c.KubernetesSpawner.hub_ip_from_service = 'jupyterhub'
+#c.KubernetesSpawner.container_image = 'danielfrg/jupyterhub-kube-ldap-nfs-singleuser:0.1'
+#c.Spawner.default_url = '/lab'
+#c.Spawner.notebook_dir = '/mnt/notebooks/%U'
+#c.KubernetesSpawner.persistent_volume_claim_name = 'jupyterhub-volume'
+#c.KubernetesSpawner.persistent_volume_claim_path = '/mnt'
