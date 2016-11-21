b39decc73f3c403ca7454f56575b65b9e357b75c
Luis Fernández Álvarez <luisfdez@users.noreply.github.com>
2016-11-21 22:05:54 +0100

    Tired...

--- example/addusers.sh
+++ example/addusers.sh
@@ -2,11 +2,12 @@
 
 IFS="
 "
-for line in `cat userlist`; do
-  test -z "$line" && continue
-  user=`echo $line | cut -f 1 -d' '`
-  echo "adding user $user"
-  useradd -m -s /bin/bash $user
-  cp -r /srv/ipython/examples /home/$user/examples
-  chown -R $user /home/$user/examples
-done
+#for line in `cat userlist`; do
+#  test -z "$line" && continue
+#  user=`echo $line | cut -f 1 -d' '`
+user=lfernand
+echo "adding user $user"
+useradd -m -s /bin/bash $user
+cp -r /srv/ipython/examples /home/$user/examples
+chown -R $user /home/$user/examples
+#done
