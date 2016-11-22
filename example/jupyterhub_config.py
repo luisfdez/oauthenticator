88d1d20aeda3f08c7bc2bb3af6f368181653e1d6
Luis Fernández Álvarez <luisfdez@users.noreply.github.com>
2016-11-22 13:31:21 +0100

    Add manual API

--- example/jupyterhub_config.py
+++ example/jupyterhub_config.py
@@ -22,8 +22,8 @@ c.KubeSpawner.start_timeout = 60 * 5
 c.KubeSpawner.singleuser_image_spec = 'yuvipanda/simple-singleuser:v1'
 
 # The spawned containers need to be able to talk to the hub through the proxy!
-c.KubeSpawner.hub_connect_ip = os.environ['HUB_CONNECT_IP']
-
+#c.KubeSpawner.hub_connect_ip = os.environ['HUB_CONNECT_IP']
+c.KubeSpawner.accessible_hub_api_url = 'http://172.30.23.15:8000/hub/api'
 c.KubeSpawner.mem_limit = '100M'
 c.KubeSpawner.mem_guarantee='100M'
 c.KubeSpawner.cpu_limit = 0.5
